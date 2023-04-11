b61d00af5a4c4e6046720e4623404940dab53c33
Vladimir Maryasov <mx.wg@yandex.ru>
2023-04-12 05:23:03 +0700

    fix room resign universal sign text

--- src/role.claimer.js
+++ src/role.claimer.js
@@ -12,9 +12,23 @@ module.exports = {
 
         target = creep.room.controller;
         let claimResult = creep.claimController(target);
-        if(claimResult == OK) {
-            if(!target.sign || target.sign.username !== "MaxWagner") {
-                creep.signController(target, "Explored by Max");
+
+        if(claimResult == OK || claimResult == -7) {
+            let text
+            switch (creep.owner.username) {
+                case 'MaxWagner':
+                    text = 'Explored by Max';
+                    break;
+                case 'DISconnect24':
+                    text = 'Owned by DIS';
+                    break;
+                case 'MonstrikSupra':
+                    text = 'NO FATE';
+                    break;
+                default:
+            }
+            if(!target.sign || target.sign.username !== creep.owner.username || target.sign.text !== text) {
+                let res = creep.signController(target, text);
             }
         } else if(claimResult == ERR_NOT_IN_RANGE) {
             creep.goTo(target);
