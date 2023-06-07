bf73965b95a073d80e637c93538e3052298dbf04
Vladimir Maryasov <mx.wg@yandex.ru>
2023-06-08 01:15:12 +0700

    simple builders

--- src/role.builder.js
+++ src/role.builder.js
@@ -20,7 +20,7 @@ module.exports = {
   name: 'builder',
   configs: function (workParts) {
     var configs = [];
-    for (let work = workParts; work >= 1; work -= 1) {
+    for (let work = workParts; work >= 2; work -= 1) {
       let carry = Math.floor(work * 2);
       let move = work + carry;
       let config = Array(work).fill(WORK).concat(Array(carry).fill(CARRY)).concat(Array(move).fill(MOVE));
@@ -28,6 +28,7 @@ module.exports = {
     }
 
     // TODO: probably more handcrafted configs for low tiers?
+    configs.push([WORK, CARRY, MOVE, MOVE]); // spawn-only config
 
     return configs;
   },
